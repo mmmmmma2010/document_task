3b4b66a4c328a27016837bd97dc3b56b46a9f068
m7md_3li <mohamed.ali.hassan@paymob.com>
2023-08-19 13:16:25 +0300

    creating file model and migrations

--- doc_app/models.py
+++ doc_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class UploadedFile(models.Model):
+    FILE_TYPES = (
+        ('image', 'Image'),
+        ('pdf', 'PDF'),
+    )
+
+    file = models.FileField(upload_to='uploads/')
+    file_type = models.CharField(max_length=10, choices=FILE_TYPES)
+
+    
